d24ef9202da02755a014c31e2f992a5e5a90fdf9
Juliusbecker <julix.becker@gmail.com>
2023-11-12 11:37:50 +0100

    Add: Tab View

--- src/pages/TrainingPlan/TrainingPlanPage.jsx
+++ src/pages/TrainingPlan/TrainingPlanPage.jsx
@@ -1,10 +1,183 @@
+import { Box, List, Tab, Tabs } from "@mui/material";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import CustomTabPanel from "../../components/CustomTabPanel";
+import PageLayout from "../../components/PageLayout";
 import TrainingBlock from "./components/TrainingBlock";
 
 function TrainingPlanPage() {
+  const mockedTrainings = [
+    {
+      courseName: "Web Development with React",
+      skills: ["HTML", "CSS", "JavaScript", "React"],
+      description: "Learn web development using the React framework.",
+      progress: 0,
+      status: 0,
+    },
+    {
+      courseName: "Python for Data Science",
+      skills: ["Python", "Data Analysis", "Machine Learning"],
+      description: "Explore Python for data science and machine learning.",
+      progress: 75,
+      status: 1,
+    },
+    {
+      courseName: "Mobile App Development with Flutter",
+      skills: ["Dart", "Flutter", "Mobile App Development"],
+      description: "Build cross-platform mobile apps using Flutter and Dart.",
+      progress: 60,
+      status: 2,
+    },
+    {
+      courseName: "JavaScript Fundamentals",
+      skills: ["JavaScript", "Front-End Development"],
+      description: "Master the fundamentals of JavaScript programming.",
+      progress: 0,
+      status: 0,
+    },
+    {
+      courseName: "Data Structures and Algorithms in C++",
+      skills: ["C++", "Data Structures", "Algorithms"],
+      description: "Learn essential data structures and algorithms using C++.",
+      progress: 80,
+      status: 1,
+    },
+    {
+      courseName: "Digital Marketing Strategy",
+      skills: ["Digital Marketing", "SEO", "Social Media"],
+      description:
+        "Create effective digital marketing strategies for businesses.",
+      progress: 70,
+      status: 2,
+    },
+    {
+      courseName: "Game Development with Unity",
+      skills: ["Unity", "C#", "Game Development"],
+      description: "Develop your own games with Unity and C#.",
+      progress: 0,
+      status: 0,
+    },
+    {
+      courseName: "Backend Development with Node.js",
+      skills: ["Node.js", "Express", "MongoDB"],
+      description:
+        "Build scalable backend applications using Node.js and Express.",
+      progress: 78,
+      status: 1,
+    },
+    {
+      courseName: "Artificial Intelligence and Machine Learning",
+      skills: ["AI", "Machine Learning", "Deep Learning"],
+      description: "Explore the fascinating world of AI and machine learning.",
+      progress: 85,
+      status: 2,
+    },
+    {
+      courseName: "Graphic Design for Beginners",
+      skills: ["Graphic Design", "Adobe Illustrator", "Visual Communication"],
+      description:
+        "Learn the basics of graphic design and visual communication.",
+      progress: 0,
+      status: 0,
+    },
+  ];
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <>
-      <TrainingBlock />
-    </>
+    <PageLayout
+      title="Training Plan"
+      viewportPage>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}>
+          <Tab
+            value={0}
+            label="Not started"
+          />
+          <Tab
+            value={1}
+            label="In Progress"
+          />
+          <Tab
+            value={2}
+            label="Done"
+          />
+        </Tabs>
+      </Box>
+      <CustomTabPanel
+        value={value}
+        index={0}>
+        <List>
+          {mockedTrainings.map((training, index) => {
+            if (training.status === 0) {
+              return (
+                <li key={uuidv4()}>
+                  <TrainingBlock
+                    courseName={training.courseName}
+                    skills={training.skills}
+                    description={training.description}
+                    progress={training.progress}
+                  />
+                </li>
+              );
+            }
+            return null;
+          })}
+        </List>
+      </CustomTabPanel>
+      <CustomTabPanel
+        value={value}
+        index={1}>
+        <List>
+          {mockedTrainings.map((training, index) => {
+            if (training.status === 1) {
+              return (
+                <li key={uuidv4()}>
+                  <TrainingBlock
+                    courseName={training.courseName}
+                    skills={training.skills}
+                    description={training.description}
+                    progress={training.progress}
+                  />
+                </li>
+              );
+            }
+            return null;
+          })}{" "}
+        </List>
+      </CustomTabPanel>
+      <CustomTabPanel
+        value={value}
+        index={2}>
+        <List>
+          {mockedTrainings.map((training, index) => {
+            if (training.status === 2) {
+              return (
+                <li key={uuidv4()}>
+                  <TrainingBlock
+                    courseName={training.courseName}
+                    skills={training.skills}
+                    description={training.description}
+                    progress={training.progress}
+                  />
+                </li>
+              );
+            }
+            return null;
+          })}{" "}
+        </List>
+      </CustomTabPanel>
+      <TrainingBlock
+        courseName="Coding Course"
+        skills={["React", "Python", "React"]}
+        description="This is a description"
+        progress={89}
+      />
+    </PageLayout>
   );
 }
 
