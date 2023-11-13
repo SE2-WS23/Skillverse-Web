4ebbc948412ae138b5a2cf38272ca03ee337338c
Nicolas Braun <76167936+NicolasKmbr@users.noreply.github.com>
2023-11-13 17:05:00 +0100

    Add: Horizontal Slider

--- src/pages/CourseFeedback/CourseFeedbackPage.jsx
+++ src/pages/CourseFeedback/CourseFeedbackPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Slider } from "@mui/material";
+import PageLayout from "../../components/PageLayout";
 
 function CourseFeedbackPage() {
-  return <div>CourseFeedbackPage</div>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <PageLayout viewportPage title="Course Feedback">
+      <Slider
+        size="medium"
+        color="primary"
+        orientation="horizontal"
+        value={value}
+        onChange={handleChange}
+        aria-label="feedback slider"
+      />
+    </PageLayout>
+  );
 }
 
 export default CourseFeedbackPage;
