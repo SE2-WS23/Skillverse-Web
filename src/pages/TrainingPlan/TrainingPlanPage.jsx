84c78b93d544150507943c5fc1446199f98a6b1a
Juliusbecker <julix.becker@gmail.com>
2023-11-13 17:28:38 +0100

    Remove: unused variable

--- src/pages/TrainingPlan/TrainingPlanPage.jsx
+++ src/pages/TrainingPlan/TrainingPlanPage.jsx
@@ -87,7 +87,7 @@ function TrainingPlanPage() {
   ];
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (_, newValue) => {
     setValue(newValue);
   };
   return (
