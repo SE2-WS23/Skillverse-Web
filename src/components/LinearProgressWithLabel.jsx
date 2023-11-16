260720a105551f33293d4a539dd80a6924e500fc
Juliusbecker <julix.becker@gmail.com>
2023-11-16 14:15:34 +0100

    Add: LinearProgress Component

---
+++ src/components/LinearProgressWithLabel.jsx
@@ -0,0 +1,21 @@
+import { Box, LinearProgress, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box display="flex" justifyContent="space-between">
+        <Typography>{props.title || ""}</Typography>
+        <Typography>{props.value || 0}%</Typography>
+      </Box>
+      <LinearProgress variant="determinate" value={props.value || 0} />
+    </Box>
+  );
+}
+
+export default LinearProgressWithLabel;
+
+LinearProgressWithLabel.propTypes = {
+  title: PropTypes.string,
+  value: PropTypes.number,
+};
