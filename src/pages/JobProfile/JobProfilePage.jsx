f7c9e2d26ce7f673846e31234019f0ff26cb56af
aanckr <a.annacker@student.xu-university.de>
2023-11-16 09:29:41 +0100

    Refactor: Item, so it's just a single component

--- src/pages/JobProfile/JobProfilePage.jsx
+++ src/pages/JobProfile/JobProfilePage.jsx
@@ -1,27 +1,39 @@
 import PageLayout from "../../components/PageLayout";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import JobProfileItem from "./components/JobProfileItem";
+import mockedJobs from "./mockData";
 
+function JobProfilePage() {
+  return (
+    <PageLayout title="Job Profile">
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        sx={{
+          margin: "115px 0 0px 115px",
+        }}
+      >
+        Add Job
+      </Button>
 
-function JobProfilePage(){
-
-    return(
-        <PageLayout title="Job Profile">
-
-            <Button 
-            variant="contained" 
-            color="primary" 
-            size="large"
-            sx={{
-                margin: "115px 0 0px 115px"
-            }}
-            >
-            Add Job
-            </Button>
-
-            <JobProfileItem/>
-            
-        </PageLayout>
-    )
+      <Grid
+        container
+        rowSpacing={6}
+        columnSpacing={10}
+        columns={16}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "50px",
+          marginBottom: "50px",
+        }}
+      >
+        {mockedJobs?.map((job) => (
+          <JobProfileItem key={job.title} job={job} />
+        ))}
+      </Grid>
+    </PageLayout>
+  );
 }
 export default JobProfilePage;
--- src/pages/JobProfile/components/JobProfileItem.jsx
+++ src/pages/JobProfile/components/JobProfileItem.jsx
@@ -1,37 +1,21 @@
 import { useTheme } from "@emotion/react";
 import { Grid, ListItem, ListItemText } from "@mui/material";
-import mockedJobs from "../mockData";
 
-function JobProfileItem() {
+function JobProfileItem({ job }) {
   return (
-    <Grid
-      container
-      rowSpacing={6}
-      columnSpacing={10}
-      columns={16}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: "50px",
-        marginBottom: "50px",
-      }}
-    >
-      {mockedJobs?.map((job) => (
-        <Grid item xs={6} key={job.title}>
-          <ListItem
-            sx={{
-              height: "100px",
-              background: useTheme().palette.primary.light,
-            }}
-          >
-            <ListItemText
-              primary={job.category}
-              secondary={job.title}
-              secondaryTypographyProps={{ align: "center" }}
-            />
-          </ListItem>
-        </Grid>
-      ))}
+    <Grid item xs={6}>
+      <ListItem
+        sx={{
+          height: "100px",
+          background: useTheme().palette.primary.light,
+        }}
+      >
+        <ListItemText
+          primary={job.category}
+          secondary={job.title}
+          secondaryTypographyProps={{ align: "center" }}
+        />
+      </ListItem>
     </Grid>
   );
 }
