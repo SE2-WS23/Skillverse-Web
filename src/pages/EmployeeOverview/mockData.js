1532eb2e4ce9b8cd523a23e0dc2afea412396b83
Juliusbecker <julix.becker@gmail.com>
2023-11-17 11:36:52 +0100

    Add: Mock data

---
+++ src/pages/EmployeeOverview/mockData.js
@@ -0,0 +1,69 @@
+const employees = [
+  {
+    name: "John Doe",
+    jobTitle: "Software Engineer",
+    email: "john.doe@example.com",
+    courses: [
+      { name: "JavaScript Fundamentals", progress: 80 },
+      { name: "React Basics", progress: 90 },
+    ],
+    profileUrl: "https://example.com/john_doe",
+    profileImageUrl: "https://ui-avatars.com/api/?name=John+Doe",
+    finishedCourses: 3,
+    ongoingCourses: 2,
+  },
+  {
+    name: "Jane Smith",
+    jobTitle: "Product Manager",
+    email: "jane.smith@example.com",
+    courses: [
+      { name: "Product Management 101", progress: 95 },
+      { name: "User Experience Design", progress: 85 },
+    ],
+    profileUrl: "https://example.com/jane_smith",
+    profileImageUrl: "https://ui-avatars.com/api/?name=Jane+Smith",
+    finishedCourses: 3,
+    ongoingCourses: 2,
+  },
+  {
+    name: "Alice Johnson",
+    jobTitle: "Data Analyst",
+    email: "alice.johnson@example.com",
+    courses: [
+      { name: "Data Analysis with Python", progress: 75 },
+      { name: "SQL for Data Science", progress: 70 },
+    ],
+    profileUrl: "https://example.com/alice_johnson",
+    profileImageUrl: "hhttps://ui-avatars.com/api/?name=Alice+Johnson",
+    finishedCourses: 3,
+    ongoingCourses: 2,
+  },
+  {
+    name: "Bob Brown",
+    jobTitle: "Marketing Specialist",
+    email: "bob.brown@example.com",
+    courses: [
+      { name: "Digital Marketing Fundamentals", progress: 88 },
+      { name: "Social Media Advertising", progress: 92 },
+    ],
+    profileUrl: "https://example.com/bob_brown",
+    profileImageUrl: "https://ui-avatars.com/api/?name=Bob+Brown",
+    finishedCourses: 3,
+    ongoingCourses: 2,
+  },
+  {
+    name: "Eva Wilson",
+    jobTitle: "HR Manager",
+    email: "eva.wilson@example.com",
+    courses: [
+      { name: "Employee Relations", progress: 70 },
+      { name: "HR Policy Development", progress: 75 },
+    ],
+    profileUrl: "https://example.com/eva_wilson",
+    profileImageUrl: "https://ui-avatars.com/api/?name=Eva+Wilson",
+    finishedCourses: 3,
+    ongoingCourses: 2,
+  },
+];
+
+export default employees;
