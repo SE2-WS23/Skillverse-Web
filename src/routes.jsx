092f832e9bc5c1f38b05b2dcbccfcee8c1c5b192
uzoma <munachimsouzoma814@gmail.com>
2023-11-19 23:15:00 +0100

    fix: added missing imports after merge from main

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,14 @@
 import AssessmentPage from "./pages/Assessment/AssessmentPage";
 import CourseFeedbackPage from "./pages/CourseFeedback/CourseFeedbackPage";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
+import EmployeeOverviewPage from "./pages/EmployeeOverview/EmployeeOverviewPage";
 import HomePage from "./pages/Home/HomePage";
 import JobProfilePage from "./pages/JobProfile/JobProfilePage";
 import LinkedInLearning from "./pages/LinkedInLearning/LinkedInLearningPage";
 import LoginPage from "./pages/Login/LoginPage";
 import TrainingPlanPage from "./pages/TrainingPlan/TrainingPlanPage";
+import SkillSetPage from "./pages/JobProfile/SkillSetPage";
+
 
 const ROUTES = [
   {
