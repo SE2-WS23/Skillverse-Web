ea9b5509db92ac4f8cea5f1c76b3e6d44b384f04
Chris <chrissy.drx@gmail.com>
2023-11-22 21:39:50 +0100

    Add: Display Markdown

--- src/pages/CodeChallenge/CodeChallengePage.jsx
+++ src/pages/CodeChallenge/CodeChallengePage.jsx
@@ -1,7 +1,10 @@
+import { Box } from "@mui/system";
 import React, { useState } from "react";
+import Markdown from "react-markdown";
 import SplitPane, { Pane } from "split-pane-react";
 import "split-pane-react/esm/themes/default.css";
 import PageLayout from "../../components/PageLayout";
+import markdown from "./mockData";
 
 function CodeChallengePage() {
   const [sizes, setSizes] = useState([
@@ -17,6 +20,7 @@ function CodeChallengePage() {
     height: "100%",
     borderLeft: "2px solid black",
     borderRight: "2px solid black",
+    overflow: "auto",
   };
 
   return (
@@ -28,12 +32,18 @@ function CodeChallengePage() {
         height="100%"
       >
         <Pane minSize={minSize} maxSize={maxSize}>
-          <div style={{ ...layoutCSS, background: "#ddd" }}>pane1</div>
+          <Box style={{ ...layoutCSS, padding: 10 }}>
+            <Markdown>{markdown}</Markdown>
+          </Box>
         </Pane>
         <Pane minSize={minSize} maxSize={maxSize}>
           <div style={{ ...layoutCSS, background: "#eee" }}>pane2</div>
         </Pane>
-        <Pane minSize={minSize} maxSize={maxSize}></Pane>
+        <Pane minSize={minSize} maxSize={maxSize}>
+          <Box sx={{ ...layoutCSS, background: "#fff" }}>
+            Code Output <br /> <i>Has to be valided by backend</i>
+          </Box>
+        </Pane>
       </SplitPane>
     </PageLayout>
   );
---
+++ src/pages/CodeChallenge/mockData.js
@@ -0,0 +1,25 @@
+const markdown = `# The Basics of Web Development
+
+Web development is an exciting field that combines creativity and technical skills. This guide provides an overview of the basics of web development.
+
+## Understanding Web Development
+
+Web development is the process of building and maintaining websites. It's the work that happens behind the scenes to make a website look great, work fast, and perform well with a seamless user experience.
+
+### What is a Web Developer?
+
+A web developer is someone who builds and maintains websites. They are responsible for the site's technical aspects, such as its performance and capacity.
+
+## Front-End Development
+
+Front-end development involves creating the parts of a website that users interact with. 
+
+### Key Technologies
+
+**HTML:** The standard markup language for creating web pages, 
+**CSS:** A stylesheet language used to control the layout and appearance of web pages, 
+**JavaScript:** A scripting language used to create dynamic content.
+
+`;
+
+export default markdown;
