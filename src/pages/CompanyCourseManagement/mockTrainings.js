a5170b8e37d2bdff490d32e3b07386c44c34c9ac
Ara-Ara-ASid-kun <sidqi.zahidi@gmail.com>
2023-11-28 22:50:19 +0100

    Created mock data files

---
+++ src/pages/CompanyCourseManagement/mockTrainings.js
@@ -0,0 +1,23 @@
+const mockTrainings = [
+    {
+      trainingNumber: 1,
+    },
+    {
+      trainingNumber: 2,
+    },
+    {
+      trainingNumber: 3,
+    },
+    {
+      trainingNumber: 4,
+    },
+    {
+      trainingNumber: 5,
+    },
+    {
+      trainingNumber: 6,
+    },
+  ];
+  
+  export default mockTrainings;
+  
---
+++ src/pages/CompanyTrainingOverview/mockCourses.js
@@ -0,0 +1,22 @@
+const mockCourses = [
+    {
+      courseNumber: 1,
+    },
+    {
+      courseNumber: 2,
+    },
+    {
+      courseNumber: 3,
+    },
+    {
+      courseNumber: 4,
+    },
+    {
+      courseNumber: 5,
+    },
+    {
+      courseNumber: 6,
+    },
+  ];
+  
+  export default mockCourses;
