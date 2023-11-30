3fbf8c46f6d2a8b7f580381b49863c41097516a7
Ara-Ara-ASid-kun <sidqi.zahidi@gmail.com>
2023-11-30 22:23:45 +0100

    Updated routes.jsx to organize pages for import

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,7 @@
 import AssessmentPage from "./pages/Assessment/AssessmentPage";
-<<<<<<< HEAD
 import CodeChallengePage from "./pages/CodeChallenge/CodeChallengePage";
 import CompanyCourseManagementPage from "./pages/CompanyCourseManagement/CompanyCourseManagementPage";
 import CompanyTrainingsOverviewPage from "./pages/CompanyTrainingOverview/CompanyTrainingsOverviewPage";
-=======
-import CompanyCourseManagementPage from "./pages/CompanyCourseManagement/CompanyCourseManagementPage";
-import CompanyTrainingsOverviewPage from "./pages/CompanyTrainingOverview/CompanyTrainingsOverviewPage";
-import CodeChallengePage from "./pages/CodeChallenge/CodeChallengePage";
->>>>>>> f486009a491d78c6eb20466bb937ba2e12ac7569
 import CourseFeedbackPage from "./pages/CourseFeedback/CourseFeedbackPage";
 import CreateJobProfilePage from "./pages/CreateJobProfile/CreateJobProfilePage";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
