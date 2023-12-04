0fd64222b99906ff4334305fb36d8313599c6a27
Ara-Ara-ASid-kun <sidqi.zahidi@gmail.com>
2023-12-04 22:41:08 +0100

    Changed variable, added category & numbers

--- src/pages/CompanyTrainingOverview/mockCourses.js
+++ src/pages/CompanyTrainingOverview/mockCourses.js
@@ -1,22 +1,54 @@
 const mockCourses = [
-    {
-      courseNumber: 1,
-    },
-    {
-      courseNumber: 2,
-    },
-    {
-      courseNumber: 3,
-    },
-    {
-      courseNumber: 4,
-    },
-    {
-      courseNumber: 5,
-    },
-    {
-      courseNumber: 6,
-    },
-  ];
-  
-  export default mockCourses;
+  {
+    courseName: 'Building Efficient CI/CD Pipelines',
+    courseCategory: 'DevOps Fundamentals',
+    trainingNumber: 1,
+  },
+  {
+    courseName: 'Protecting Your Digital Assets',
+    courseCategory: 'Cybersecurity Essentials',
+    trainingNumber: 2,
+  },
+  {
+    courseName: 'Mastering Modern Web Technologies',
+    courseCategory: 'Full Stack Web Development',
+    trainingNumber: 3,
+  },
+  {
+    courseName: 'Deploying and Managing Scalable Cloud Solutions',
+    courseCategory: 'Cloud Computing Mastery',
+    trainingNumber: 4,
+  },
+  {
+    courseName: 'Analyzing Big Data for Informed Decisions',
+    courseCategory: 'Data Science Bootcamp',
+    trainingNumber: 5,
+  },
+  {
+    courseName: 'Streamlining Workflows for Success',
+    courseCategory: 'Agile Project Management',
+    trainingNumber: 6,
+  },
+  {
+    courseName: 'Understanding Predictive Analytics',
+    courseCategory: 'Machine Learning Foundations',
+    trainingNumber: 7,
+  },
+  {
+    courseName: 'Securing and Optimizing Networks',
+    courseCategory: 'Network Administration Basics',
+    trainingNumber: 8,
+  },
+  {
+    courseName: 'Creating Innovative Mobile Experiences',
+    courseCategory: 'Mobile App Development Workshop',
+    trainingNumber: 9,
+  },
+  {
+    courseName: 'Ensuring Quality IT Services',
+    courseCategory: 'IT Service Management(ITSM) Certification',
+    trainingNumber: 10,
+  },
+];
+
+export default mockCourses;
