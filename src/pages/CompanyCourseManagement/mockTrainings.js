f814d2b80361103f06f6f34705277a1b22857dad
Ara-Ara-ASid-kun <sidqi.zahidi@gmail.com>
2023-12-04 22:40:24 +0100

    Changed variables, added categories & examples

--- src/pages/CompanyCourseManagement/mockTrainings.js
+++ src/pages/CompanyCourseManagement/mockTrainings.js
@@ -1,23 +1,44 @@
 const mockTrainings = [
-    {
-      trainingNumber: 1,
-    },
-    {
-      trainingNumber: 2,
-    },
-    {
-      trainingNumber: 3,
-    },
-    {
-      trainingNumber: 4,
-    },
-    {
-      trainingNumber: 5,
-    },
-    {
-      trainingNumber: 6,
-    },
-  ];
-  
-  export default mockTrainings;
-  
+  {
+    trainingName: 'DevOps Fundamentals',
+    trainingCategory: 'Software Development',
+  },
+  {
+    trainingName: 'Cybersecurity Essentials',
+    trainingCategory: 'Cybersecurity',
+  },
+  {
+    trainingName: 'Full Stack Web Development',
+    trainingCategory: 'Web Development',
+  },
+  {
+    trainingName: 'Cloud Computing Mastery',
+    trainingCategory: 'Cloud Computing',
+  },
+  {
+    trainingName: 'Data Science Bootcamp',
+    trainingCategory: 'Data Science',
+  },
+  {
+    trainingName: 'Agile Project Management',
+    trainingCategory: 'Agile Methodologies',
+  },
+  {
+    trainingName: 'Machine Learning Foundations',
+    trainingCategory: 'Machine Learning',
+  },
+  {
+    trainingName: 'Network Administration Basics',
+    trainingCategory: 'Network Administration',
+  },
+  {
+    trainingName: 'Mobile App Development Workshop',
+    trainingCategory: 'Mobile App Development',
+  },
+  {
+    trainingName: 'IT Service Management(ITSM) Certification',
+    trainingCategory: 'IT Service Management',
+  },
+];
+
+export default mockTrainings;
